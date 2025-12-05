09d5c6d4ce223fa43ce55fd51d19328562bdf078
jon green <jongreen1996@gmail.com>
2025-12-05 14:41:28 +0000

    Add copilot to nvim config

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+      },
+      panel = { enabled = false },
+    },
+  },
+}
